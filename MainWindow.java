ad08d56be98d4278b9e160bef32e4294d2fa25a0
Timkamania <33117641+Timkamania@users.noreply.github.com>
2017-11-03 09:44:09 +1000

    Add files via upload

---
+++ MainWindow.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class MainWindow extends JFrame {
+
+    public MainWindow(){
+        setSize(300,300);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        MainWindow mw = new MainWindow();
+    }
+}
